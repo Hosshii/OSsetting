3e7f9e14cf526a110c45457f0a0bf5ed5bb771ae
WistreHosshii <sao_heath6147.wistre@icloud.com>
2019-08-21 16:09:16 +0900

    setup.sh

---
+++ Makefile
@@ -0,0 +1,1 @@
+	include ~/Workspace/hariboteOS/make.txt
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ROOT=$(pwd)
+ZISAKU="30nichideosjisaku"
+
+#clone
+#git clone https://github.com/tatsumack/30nichideosjisaku.git
+
+#make dir
+cd $ROOT/$ZISAKU
+find . -type d -path '*day*'  |xargs -I {} sh -c "cd $ROOT;mkdir -p ./src/{};"
+
+#copy file
+cd $ROOT/src
+find . -type d -mindepth 2 -maxdepth 2 |xargs -I {} cp ../Makefile {}/Makefile
+
