d3219246aa05af9ce722de77466b776c1a14df84
WistreHosshii <sao_heath6147.wistre@icloud.com>
2019-08-23 14:15:01 +0900

    fix setup and copy

--- copy.sh
+++ copy.sh
@@ -2,5 +2,7 @@
 
 cd src
 echo -n "copying files..."
-find . -type d -mindepth 2 -maxdepth 2 |xargs -I {} cp ../$1 {}/$1 
+for x in "$@";do
+    find . -type d -mindepth 2 -maxdepth 2 |xargs -I {} \cp -rf ../$x {}/$x
+done
 echo "done."
--- setup.sh
+++ setup.sh
@@ -13,7 +13,4 @@ find . -type d -path '*day*'  |xargs -I {} sh -c "cd $ROOT;mkdir -p ./src/{};"
 echo "done."
 
 #copy file
-cd $ROOT/src
-echo -n "copying files..."
-find . -type d -mindepth 2 -maxdepth 2 |xargs -I {} cp ../Makefile {}/Makefile;cp ../copy.sh {};cp ../pull.sh {};cp ../convert.sh {}
-echo "done."
+./copy.sh convert.sh pull.sh Makefile
