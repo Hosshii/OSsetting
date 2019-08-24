1a019ed87b84dd540cc28a2aa3d972c3548740fd
WistreHosshii <sao_heath6147.wistre@icloud.com>
2019-08-25 01:26:19 +0900

    use 正規表現

--- convert.sh
+++ convert.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-CONVERT=$(file ./* |grep "Non-ISO extended-ASCII text" | awk '{print $1}'|sed -e "s/:\$//")
+if [ $# -eq 0 ];then
+    CONVERT=$(file ./* |grep "Non-ISO extended-ASCII text" |grep -ve ".*orig$"| awk '{print $1}'|sed -e "s/:\$//")
+else
+    CONVERT=$@
+fi
 
 echo "converting files..."
 for f in $CONVERT;do
